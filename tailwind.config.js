014f4f159d5ab9834fae18685533740ffe483510
Roba Nath Basnet <robanbth@gmail.com>
2022-12-07 09:24:04 +0600

    setup update to support TypeScript

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+	darkMode: false,
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
